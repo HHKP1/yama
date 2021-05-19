660e0afee0cb176efd207713a0d3b9a72db48c6b
hhkp <s.volodymyr82@gmail.com>
2021-05-19 21:52:01 +0300

    upd: pbl3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/src/public/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/' : '',
 	devServer: {
 		clientLogLevel: 'info',
-		proxy: 'https://tala.cloudi.es/routes/9373315876/front/',
+		proxy: 'http://localhost:8080',
 		// proxy: {
 		// 	'^/api': {
 		// 		target: 'https://tala.cloudi.es/routes/9373315876/front/',
@@ -14,11 +14,4 @@ module.exports = {
 	// assetsDir: './assets/',
 	lintOnSave: false,
 	productionSourceMap: false
-	// pages: {
-	// 	index: {
-	// 		entry: 'src/main.js',
-	// 		template: 'src/public/index.html',
-	// 		filename: 'index.html',
-	// 	},
-	// },
 }
