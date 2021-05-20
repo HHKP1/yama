6f30414835a12927c72197183898b67c20598de1
hhkp <s.volodymyr82@gmail.com>
2021-05-20 15:22:35 +0300

    changed assetsDir

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	// 	// 	},
 	// },
 	outputDir: './public/',
-	assetsDir: '/routes/9373315876/front/asstes',
+	assetsDir: './routes/9373315876/front/asstes',
 	lintOnSave: false,
 	productionSourceMap: false
 }
