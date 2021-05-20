ff0630de2afd462edfcdb6bd55b2ab859806b0f6
hhkp <s.volodymyr82@gmail.com>
2021-05-20 15:19:18 +0300

    changed assets dir

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-	devServer: {
-		clientLogLevel: 'info',
-		proxy: 'http://localhost:8080',
-		// proxy: {
-		// 	'^/api': {
-		// 		target: 'https://tala.cloudi.es/routes/9373315876/front/',
-		// 		ws: false,
-		// 		changeOrigin: true
-		// 	},
-	},
+	// devServer: {
+	// 	clientLogLevel: 'info',
+	// 	proxy: 'http://localhost:8080',
+	// 	// proxy: {
+	// 	// 	'^/api': {
+	// 	// 		target: 'https://tala.cloudi.es/routes/9373315876/front/',
+	// 	// 		ws: false,
+	// 	// 		changeOrigin: true
+	// 	// 	},
+	// },
 	outputDir: './public/',
-	assetsDir: './asstes',
+	assetsDir: '/routes/9373315876/front/asstes',
 	lintOnSave: false,
 	productionSourceMap: false
 }
