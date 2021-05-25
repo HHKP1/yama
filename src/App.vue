16c230e10c769e39b81194781349863902e3e9d1
hhkp <s.volodymyr82@gmail.com>
2021-05-25 05:30:55 +0300

    components: FormSelect, FormInput, Pagination; styles: section how_it_works, collecton_defects; methods: apiGet

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<main>
 			<div id="nav">
-				<a href="/">
+				<a class="logo_lonk" href="/">
 					<img id="logo" src="./assets/img/logo_ukr_yama.png" alt="Logo">
 				</a>
 				<div class='nav_links' v-for="(item, index) in this.navItems" :key="item.name" :data-index="index">
@@ -142,6 +142,142 @@
 					</div>
 				</div>
 			</section>
+			<section class="collection_defects">
+				<div class="container_works">
+					<h2 class="work-item">Як це працює</h2>
+					<div class="container_defects">
+						<div class="defect_filters">
+							<p class="filter_title" style="text-align:left;">фільтри</p>
+							<div class="filters_block">
+								<FormInput
+									prefix=""
+									title=""
+									label=""
+									placeholder="Пошук дефектів за адресою"
+									v-model="search_by_adress"
+									class="form-control"
+									type="search"
+								/>
+								<FormInput
+									title=""
+									label=""
+									placeholder="Дата розміщення дефекту"
+									v-model="search_by_date"
+									class="form-control"
+									type="date"
+								/>
+								<FormInput
+									title=""
+									label=""
+									placeholder="Тип дефекту"
+									v-model="search_by_type"
+									class="form-control"
+									type="text"
+								/>
+								<FormInput
+									title=""
+									label=""
+									placeholder="Місце розташування дефекту"
+									v-model="search_by_location_place"
+									class="form-control"
+									type="text"
+								/>
+								<FormInput
+									title=""
+									label=""
+									placeholder="Статус дефекту"
+									v-model="search_by_status"
+									class="form-control"
+									type="text"
+								/>
+								<div class="input_group">
+									<p>
+										<label for="customcheck">
+											<input type="checkbox" id="customcheck" @click="selfFilters = !selfFilters" v-model="selfFilters" class="filled-in"/>
+											<span class="custom_checkbox">Показувати тільки мої дефекти</span>
+										</label>
+									</p>
+								</div>
+								<button class="btn outline_button">Показати на мапі</button>
+								<button class="btn custom_button">Показати</button>
+							</div>
+						</div>
+						<div class="defect_content">
+							<div class="defect_filter_count">
+								<div class="defect_filter_item">
+									<p class="defect_title">Загалом дефектів знайдено: <span>{{ dfCard.length }}</span></p>
+									<p class="defect_title">Кількість регіонів: <span>{{ dfCard.region_id }}</span></p>
+								</div>
+							</div>
+							<div class="sorted_item">
+								<p class="sorted_title">Показані останні дефекти зі змінами (за замовчуванням)</p>
+								<FormSelect
+									title=""
+									label=""
+									placeholder=""
+									:options="options.sort_by_options"
+									v-model="sort_by"
+									class="form-control_outline"
+									type="search"
+								/>
+							</div>
+							<div class="grid-container">
+								<div class="defect_card" v-for='(card, idx) of dfCard' :key='idx'>
+									<div class="defect_image">
+										<img class="card_image" :src="card.photo[0].url" alt="">
+										<p class="defect_date">{{ new Date(card.photo[0].timestamp) | moment("DD.MM.YY в HH:mm") }}</p>
+									</div>
+									<div class="defect_info">
+										<div class="defect_status">
+											<div class="status_item">
+												<span>Новий</span>
+											</div>
+											<div class="status_comments" :title="'Кількість коментарів: ' + card.comment.length">
+												<p class="status_count">{{ card.comment.length }}</p>
+												<img src="./assets/img/icons/bx_bx-comment-detail.svg" alt="">
+											</div>
+										</div>
+										<p class="defect_adress" :title="card.address">{{ card.address }}</p>
+									</div>
+								</div>
+							</div>
+							<!-- <div class="card_pagination">
+								<div class="pagination_control">
+
+								</div>
+							</div> -->
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="pre_footer">
+				<div class="footer_container">
+					<div class="main-hero head-hero">
+						<div class="hero-title">
+							<p class="hero-item">Дороги – наша відповідальність, контролюй це!</p>
+						</div>
+					</div>
+					<div class="button_container">
+						<button class="btn outline_button">Допомогти проєкту</button>
+						<button class="btn custom_button">Додати дефект</button>
+					</div>
+				</div>
+			</section>
+			<section class="footer">
+				<div id="footer_nav">
+					<div class="footer_logo">
+						<a href="/">
+							<img id="logo" src="./assets/img/logo_ukr_yama.png" alt="Logo">
+						</a>
+						<p class="trade_mark">УкрЯма&copy;{{ new Date() | moment("YYYY") }}</p>
+					</div>
+					<div class="footer_links">
+						<div class='nav_links' v-for="(item, index) in this.navItemsFooter" :key="item.name" :data-index="index">
+						<router-link :to='{path: item.path}'>{{ item.text }}</router-link>
+					</div>
+					</div>
+				</div>
+			</section>
 			<router-view></router-view>
 		</main>
 	</div>
@@ -150,12 +286,15 @@
 <script>
 import Vue from 'vue';
 import './assets/css/main.css';
-import FormInput from './components/FormInput'
-import M from 'materialize-css/dist/js/materialize.min';
+import FormInput from './components/FormInput';
+import FormSelect from './components/FormSelect';
+import defectCards from './mock_data';
+// import M from 'materialize-css/dist/js/materialize.min';
 export default {
 	name: 'App',
 	components: {
-		FormInput
+		FormInput,
+		FormSelect
 	},
 	data() {
 		return {
@@ -166,7 +305,7 @@ export default {
 					path: '/about'
 				},
 				{
-					name: 'Team',
+					name: 'FAQ',
 					text: 'FAQ',
 					path: '/faq'
 				},
@@ -176,23 +315,111 @@ export default {
 					path: '/contacts'
 				},
 			],
-			search: ''
+			navItemsFooter: [
+				{
+					name: 'About',
+					text: 'Про проєкт',
+					path: '/about'
+				},
+				{
+					name: 'Statistic',
+					text: 'Статистика',
+					path: '/statistic'
+				},
+				{
+					name: 'Dowloads',
+					text: 'завантаження',
+					path: '/downloads'
+				},
+				{
+					name: 'FAQ',
+					text: 'FAQ',
+					path: '/faq'
+				},
+				{
+					name: 'Contacts',
+					text: 'Контакти',
+					path: '/contacts'
+				},
+			],
+			options: {
+				sort_by_options: [
+					{ label: 'Найновіщі', value: 'newest' },
+					{ label: 'Старіщі', value: 'oldest' },
+				],
+			},
+			orgInfo: [],
+			cardCount: 10,
+			apiURL: '/api',
+			search: '',
+			search_by_adress: '',
+			search_by_date: '',
+			search_by_location_place: '',
+			search_by_type: '',
+			search_by_status: '',
+			sort_by: '',
+			selfFilters: false,
 		}
 	},
 	created() {
-		Vue.prototype.$API = this
+		Vue.prototype.$API = this;
 	},
 	mounted() {
-		M.AutoInit();
-		M.updateTextFields();
+		this.loadCard(true);
+		// M.AutoInit();
+		// M.updateTextFields();
 	},
-	computed: {},
+	methods: {
+		apiGET: async function(endpoint){
+			let headers={
+				'Accept': 'application/json',
+			};
+			// if(auth)
+			// 	headers["Authorization"]=`Bearer ${this.token}`;
+			let response = await fetch(this.apiURL + endpoint, {
+				method: 'GET',
+				headers: headers
+			});
+			let data = await response.json();
+			return data;
+		},
+		async loadCard(change){
+			this.pendingUpdate = this.$API.apiGET("/defects?limit=8");
+			let result = await this.pendingUpdate;
+			console.log(result);
+			this.orgInfo=result;
+			return this.orgInfo;
+		}
+	},
+	computed: {
+		defectCard(){
+			return defectCards.map(card => {
+				// console.log(card);
+				return {
+					address: card.address,
+					photo: card.photos,
+					// status: card.case_status,
+					comment: card.comments,
+				}
+			})
+		},
+		dfCard(){
+			return this.orgInfo.map(card => {
+				console.log(card);
+				return {
+					address: card.address,
+					photo: card.photos,
+					// status: card.case_status,
+					comment: card.comments[0].text,
+				}
+			});
+		},
+	},
 	watch: {}
 }
 </script>
 
 <style>
-	@import "~materialize-css/dist/css/materialize.css/";
 
 	section {
 		width: 100%;
@@ -220,18 +447,15 @@ export default {
 	}
 
 	.nav_links {
-		margin: 0 15px !important
+		margin: 0 15px !important;
+		white-space: nowrap;
 	}
 
-	#nav a {
+	a {
 		font-weight: bold;
 		color: #2c3e50;
 		font-size: .9rem;
 		text-decoration: none;
-	}
-
-	#nav a:hover {
-		transition: all .2s ease-in-out;
 	}
 
 	#nav a.router-link-exact-active {
@@ -243,16 +467,32 @@ export default {
 	}
 
 	.custom_button {
+		font: 700 1.1rem 'Montserrat';
 		background-color: var(--button-default);
 		font-size: .9rem;
 		font-weight: bold;
 		text-transform: none;
 		max-width: 239px;
+		width: 100%;
+		margin:10px 0;
+	}
+	.outline_button{
+		font: 700 1.1rem 'Montserrat';
+		background-color: var(--button-outline);
+		font-size: .9rem;
+		font-weight: bold;
+		text-transform: none;
+		max-width: 239px;
+		width: 100%;
+		color: var(--color-black);
+		border: 1px solid var(--color-black);
+		margin:5px 0;
 	}
 	.hero-btn{
 		font: 700 1.1rem 'Montserrat';
 		width: 100%;
 		max-width: 176px;
+		text-transform: none;
 	}
 	.underline-btn{
 		border:none;
@@ -479,5 +719,169 @@ export default {
 	}
 	.justify_end{
 		justify-content: flex-end !important;
+	}
+	.container_defects{
+		display: flex;
+	}
+	.defect_content{
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		flex-wrap: wrap;
+		width: 100%;
+		padding: 0 10px;
+	}
+	.defect_filters{
+		flex: 0 1 30%;
+	}
+	.sorted_item{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		padding: 0 16px;
+	}
+	.defect_adress{
+		font: 400 .8rem 'Montserrat', sans-serif;
+		text-align: left;
+		/* text-overflow: ellipsis; */
+		/* white-space: nowrap; */
+		max-height: 40px;
+		overflow: hidden;
+		cursor: pointer;
+	}
+	.input_group{
+		display: flex;
+		flex: 0 1 100%;
+	}
+	.grid-container {
+		width: 95%;
+		position: relative;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(165px, 1fr));
+		/* grid-template-rows: 1fr 1fr; */
+		gap: 18px 8px;
+	}
+	.defect_card{
+		display: flex;
+		flex-flow: column;
+		width: 100%;
+		max-width: 160px;
+		border: 1px solid var(--color-gray);
+		border-radius: 4px;
+		min-height: 200px;
+		max-height: 200px;
+		padding: 10px;
+		place-self: center;
+	}
+	.defect_filter_count{
+		width: 100%;
+		position: relative;
+		padding: 0 16px;
+	}
+	.defect_filter_item{
+		display: flex;
+		flex-flow: row wrap;
+		white-space: nowrap;
+		width: 100%;
+	}
+	.defect_title{
+		font: 700 1rem 'Montserrat', sans-serif;
+		margin: 0 6px;
+	}
+	.defect_title > span{
+		font: 400 1.2rem 'Montserrat', sans-serif;
+		color: var(--color-gray);
+	}
+	.status_item{
+		background: var(--status-color);
+		border-radius: 4px;
+		color: var(--color-white);
+		padding: 0 4px;
+		font-size: .8rem;
+		margin: 4px 0;
+	}
+	.status_comments{
+		display: flex;
+		cursor: pointer;
+	}
+	.status_count{
+		font: 400 .8rem 'Montserrat', sans-serif;
+		margin: 0 4px 0 0;
+	}
+	.defect_image{
+		position: relative;
+	}
+	.defect_status{
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.defect_date{
+		color: white;
+		position: absolute;
+		display: flex;
+		font-size: .8rem;
+		left: 4%;
+		bottom: 4%;
+	}
+	.card_image{
+		object-fit: cover;
+		width: 100%;
+		max-width: 140px;
+		max-height: 120px;
+		min-width: 140px;
+		min-height: 120px;
+	}
+	[type="checkbox"].filled-in:not(:checked) + span:not(.lever):after {
+		height: 20px;
+		width: 20px;
+		background-color: transparent;
+		border: 1px solid var(--color-gray);
+		top: 0px;
+		z-index: 0;
+	}
+	.custom_checkbox{
+		font: 400 .8rem 'Montserrat' !important;
+		display: flex !important;
+		align-items: center
+	}
+	/*  Pre-Footer  */
+	.footer_container{
+		display: flex;
+		justify-content: space-between;
+		padding: 40px 20px;
+	}
+	.pre_footer{
+		margin: 20px 0;
+		background: var(--background-color-prefooter);
+	}
+	.button_container{
+		display: flex;
+		flex: 0 1 44%;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		flex-flow: row-reverse;
+	}
+	#footer_nav{
+		padding: 15px 15px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+	}
+	.footer_links{
+		display: flex;
+		justify-content: center;
+		flex: 1 1 auto;
+	}
+	.footer_logo{
+		display: flex;
+		align-items: center;
+	}
+	.nav_links > a:hover{
+		transition: all .4s ease-in-out;
+		border-bottom: 2px solid var(--color-red);
 	}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-	// devServer: {
-	// 	clientLogLevel: 'info',
-	// 	proxy: 'http://localhost:8080',
-	// 	// proxy: {
-	// 	// 	'^/api': {
-	// 	// 		target: 'https://tala.cloudi.es/routes/9373315876/front/',
-	// 	// 		ws: false,
-	// 	// 		changeOrigin: true
-	// 	// 	},
-	// },
+	devServer: {
+		// clientLogLevel: 'info',
+		// proxy: 'http://localhost:8080',
+		proxy: {
+			'^/api': {
+				target: 'https://tala.cloudi.es/routes/95a4b653d1/',
+				ws: false,
+				changeOrigin: true
+			},
+		}
+	},
 	outputDir: './public/',
 	assetsDir: './assets',
 	lintOnSave: false,
