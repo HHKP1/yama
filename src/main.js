7bf200d2a68a5a41afe95431da631aa0fdb890a9
hhkp <s.volodymyr82@gmail.com>
2021-07-08 16:55:25 +0300

    BUILD: tads, component: DatePicker

--- src/main.js
+++ src/main.js
@@ -31,16 +31,17 @@ Vue.use(VueRouter)
 Vue.use(VueMq, { breakpoints: { sm: 450, md: 1135, lg: Infinity }, defaultBreakpoint: 'lg' });
 
 const routes = [
-	{ path: './', component: CollectionList, props: true },
-	{ path: './collections/', component: CollectionList, props: true },
+	{ path: '/', component: CollectionList, props: true },
+	{ path: '/collections/', component: CollectionList, props: true },
 	// { path: '/collections/:listType', component: CollectionList },
-	{ path: './collections/defect/:id', component: DefectView, props: true },
-	{ path: './404', component: NotFoundPage },
+	{ path: '/collections/defect/:id', component: DefectView, props: true },
+	{ path: '/404', component: NotFoundPage },
 ]
 
 const router = new VueRouter({
 	routes, // short for `routes: routes`
 	mode: 'history',
+	base: '/ukr-yama/public/index.html',
 });
 
 Vue.config.productionTip = false;
