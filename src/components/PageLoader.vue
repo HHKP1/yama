e545675df54e8d838f8dd581a9c70a1964c38f92
hhkp <s.volodymyr82@gmail.com>
2021-09-01 13:09:13 +0300

    BUILD: Preloader test

--- src/components/PageLoader.vue
+++ src/components/PageLoader.vue
@@ -37,25 +37,40 @@ export default {
 		}
 	},
 	mounted() {
-		// setTimeout(this.toggleShow, 5000)
+		setTimeout(this.toggleShow, 0)
 		document.onreadystatechange = async () => {
 			if(await document.readyState == 'complete'){
 				this.isLoaded=true;
 			}
 		}
 
-		const leters = this.$refs;
-		const timeline = gsap.timeline();
-		Object.values(leters).forEach(l => {
-			// console.log(l);
-			this.staggerLetters.push(l);
-		});
+		// const leters = this.$refs;
+		// const timeline = gsap.timeline();
+		// Object.values(leters).forEach(l => {
+		// 	// console.log(l);
+		// 	this.staggerLetters.push(l);
+		// });
 
-		timeline.from(this.staggerLetters, { autoAlpha: 0, y: 10, scale: 0.9, duration: 0.7, ease: 'power4.Out', stagger: 0.09 }, '+=3.3');
-		timeline.to(this.staggerLetters, { autoAlpha: 0, duration: 0.8, ease: 'power4.Out', stagger: 0.09 }, '+=0.7');
+		// // eslint-disable-next-line no-floating-decimal
+		// timeline.from(this.staggerLetters, { autoAlpha: 0, y: 10, scale: .9, duration: .7, ease: 'power4.Out', stagger: 0.09 }, '+=3.3');
+		// // eslint-disable-next-line no-floating-decimal
+		// timeline.to(this.staggerLetters, { autoAlpha: 0, duration: .8, ease: 'power4.Out', stagger: 0.09 }, '+=0.7');
 		// setTimeout(this.loaderAnime(), 1000)
 	},
-	methods: {}
+	methods: {
+		toggleShow(){
+			setTimeout(() => {
+				const leters = this.$refs;
+				const timeline = gsap.timeline();
+				Object.values(leters).forEach(l => {
+					// console.log(l);
+					this.staggerLetters.push(l);
+				});
+				timeline.from(this.staggerLetters, { autoAlpha: 0, y: 10, scale: 0.9, duration: 0.7, ease: 'power4.Out', stagger: 0.09 }, '+=3.3');
+				timeline.to(this.staggerLetters, { autoAlpha: 0, duration: 0.8, ease: 'power4.Out', stagger: 0.09 }, '+=0.7');
+			}, 0)
+		}
+	}
 }
 </script>
 
