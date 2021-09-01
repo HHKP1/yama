d697d214c89edb0a2b18452daa67ce165c641e59
hhkp <s.volodymyr82@gmail.com>
2021-09-01 15:20:43 +0300

    BUILD: Preloader test

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+				loader: "url?limit=10000&mimetype=application/font-woff",
 				// More information here https://webpack.js.org/guides/asset-modules/
 				type: "asset",
 			},
