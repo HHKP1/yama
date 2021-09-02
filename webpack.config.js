4feedb041b711a7660ab881e0e2642912be4aed4
hhkp <s.volodymyr82@gmail.com>
2021-09-02 13:05:43 +0300

    BUILD: show gMap (mobile)

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-				// More information here https://webpack.js.org/guides/asset-modules/
-				type: "asset",
-			},
-		],
-	},
-};
+// module.exports = {
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.css$/i,
+// 				use: ["style-loader", "css-loader"],
+// 			},
+// 			{
+// 				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+// 				// More information here https://webpack.js.org/guides/asset-modules/
+// 				type: "asset",
+// 			},
+// 		],
+// 	},
+// };
