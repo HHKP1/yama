fddf8d849b39f9e433a0678079cd52b3470e8724
hhkp <s.volodymyr82@gmail.com>
2021-09-20 17:20:09 +0300

    og tags test

--- src/App.vue
+++ src/App.vue
@@ -489,20 +489,19 @@ export default {
 		Defects,
 		PageLoader,
 	},
-	metaInfo() {
-		return {
-			title: "Ukr-Yama",
-			meta: [
-				{ name: 'description', content: 'УкрЯма – це інструмент громадського контролю за утриманням доріг' },
-				{ property: 'og:title', content: "УкрЯма – це інструмент громадського контролю за утриманням доріг" },
-				{ property: 'og:site_name', content: 'УкрЯма' },
-				{ property: 'fb:app_id', content: 366159005244899 },
-				{ property: 'og:url', content: 'https://tala.cloudi.es/routes/9373315876/front/#/' },
-				// { property: 'og:image', content: this.apiURL + this.endpoint },
-				{ property: 'og:type', content: 'website' },
-				{ name: 'robots', content: 'index,follow' }
-			]
-		}
+	metaInfo: {
+		title: "Ukr-Yama",
+		meta: [
+			{ name: 'description', content: 'УкрЯма – це інструмент громадського контролю за утриманням доріг' },
+			{ property: 'og:description', content: 'УкрЯма – це інструмент громадського контролю за утриманням доріг' },
+			{ property: 'og:title', content: "УкрЯма – це інструмент громадського контролю за утриманням доріг" },
+			{ property: 'og:site_name', content: 'УкрЯма' },
+			{ property: 'fb:app_id', content: 366159005244899 },
+			{ property: 'og:url', content: 'https://tala.cloudi.es/routes/9373315876/front/#/' },
+			// { property: 'og:image', content: this.apiURL + this.endpoint },
+			{ property: 'og:type', content: 'website' },
+			{ name: 'robots', content: 'index,follow' }
+		]
 	},
 	data() {
 		return {
