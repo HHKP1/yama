c768433b9f37a512680d86195e0a1578115b0d5b
hhkp <s.volodymyr82@gmail.com>
2021-09-21 15:44:03 +0300

    TEST: router mode history

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import Meta from 'vue-meta';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 import VueMq from 'vue-mq';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
@@ -51,7 +51,7 @@ Vue.use(Meta, {
 })
 
 Vue.use(VueAxios, axios);
-Vue.use(gsap);
+// Vue.use(gsap);
 Vue.use(VueRouter)
 Vue.use(VueMq, { breakpoints: { sm: 450, md: 1135, lg: Infinity }, defaultBreakpoint: 'lg' });
 Vue.use(VueGoogleMaps, { load: { key: GOOGLE_API_KEY, region: 'UA', language: 'uk-UA' }, libraries: 'places', installComponents: true });
